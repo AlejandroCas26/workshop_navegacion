f52eaa3eb3eeb3d3000f409ce72312203698a0f5
AlejandroCas26 <156716033+AlejandroCas26@users.noreply.github.com>
2024-02-05 20:21:01 -0600

    Creando los tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 return MaterialApp(
   home: DefaultTabController(
     length: 3,
-    child: Scaffold(),
+    child: Scaffold(
+      appBar: AppBar(
+        bottom: const TabBar(
+          tabs: [
+            Tab(icon: Icon(Icons.directions_car)),
+            Tab(icon: Icon(Icons.directions_transit)),
+            Tab(icon: Icon(Icons.directions_bike)),
+          ],
+        ),
+      ),
+    ),
   ),
 );
